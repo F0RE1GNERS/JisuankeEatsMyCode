03b6fbfa0f44a199d3f445619f43c0f9f975065b
Scott Chang <scottyugochang@hotmail.com>
2018-09-08 18:26:09 +0800

    Create f.cpp

---
+++ 1556/f.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+#ifdef zerol
+#define dbg(args...) do { cout << "DEBUG: " << #args << " -> "; err(args); } while (0)
+#else
+#define dbg(...)
+#endif
+#ifdef ONLINE_JUDGE
+#define assert(condition) if (!(condition)) { int x = 1, y = 0; cout << x / y << endl; }
+#endif
+void err() { cout << "" << endl; }
+template<template<typename...> class T, typename t, typename... Args>
+void err(T<t> a, Args... args) { for (auto x: a) cout << x << ' '; err(args...); }
+template<typename T, typename... Args>
+void err(T a, Args... args) { cout << a << ' '; err(args...); }
+// -----------------------------------------------------------------------------
+
+const int INF = 1E9;
+
+struct E {
+    int to, cp;
+    E(int to, int cp): to(to), cp(cp) {}
+};
+
+struct Dinic {
+    static const int M = 1E5 * 5;
+    int m, s, t;
+    vector<E> edges;
+    vector<int> G[M];
+    int d[M];
+    int cur[M];
+
+    void init(int n, int s, int t) {
+        this->s = s; this->t = t;
+        for (int i = 0; i <= n; i++) G[i].clear();
+        edges.clear(); m = 0;
+    }
+
+    void addedge(int u, int v, int cap) {
+        edges.emplace_back(v, cap);
+        edges.emplace_back(u, 0);
+        G[u].push_back(m++);
+        G[v].push_back(m++);
+    }
+
+    bool BFS() {
+        memset(d, 0, sizeof d);
+        queue<int> Q;
+        Q.push(s); d[s] = 1;
+        while (!Q.empty()) {
+            int x = Q.front(); Q.pop();
+            for (int& i: G[x]) {
+                E &e = edges[i];
+                if (!d[e.to] && e.cp > 0) {
+                    d[e.to] = d[x] + 1;
+                    Q.push(e.to);
+                }
+            }
+        }
+        return d[t];
+    }
+
+    int DFS(int u, int cp) {
+        if (u == t || !cp) return cp;
+        int tmp = cp, f;
+        for (int& i = cur[u]; i < G[u].size(); i++) {
+            E& e = edges[G[u][i]];
+            if (d[u] + 1 == d[e.to]) {
+                f = DFS(e.to, min(cp, e.cp));
+                e.cp -= f;
+                edges[G[u][i] ^ 1].cp += f;
+                cp -= f;
+                if (!cp) break;
+            }
+        }
+        return tmp - cp;
+    }
+
+    int go() {
+        int flow = 0;
+        while (BFS()) {
+            memset(cur, 0, sizeof cur);
+            flow += DFS(s, INF);
+        }
+        return flow;
+    }
+} DC;
+
+int main() {
+    int n, m, k, cas = 0;
+    while (cin >> n >> m >> k) {
+        int l, r; cin >> l >> r;
+        int S = n + m + 1, T = n + m + 2;
+        int SP = n + m + 3, TP = n + m + 4;
+        DC.init(n + m + 10, SP, TP);
+        DC.addedge(T, S, INF);
+        FOR (i, 1, n + 1) {
+            DC.addedge(S, i, r - l);
+            DC.addedge(SP, i, l);
+            DC.addedge(S, TP, l);
+        }
+        FOR (i, 1, m + 1) {
+            DC.addedge(n + i, T, r - l);
+            DC.addedge(SP, T, l);
+            DC.addedge(n + i, TP, l);
+        }
+        while (k--) {
+            int u, v; scanf("%d%d", &u, &v);
+            DC.addedge(u, n + v, 1);
+        }
+        printf("Case %d: ", ++cas);
+        if (DC.go() == l * (n + m))
+            cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
