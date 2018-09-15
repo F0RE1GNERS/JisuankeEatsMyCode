db6c6a77c11bcb2d616312f7235490aa0bba5cca
Scott Chang <scottyugochang@hotmail.com>
2018-09-15 18:11:45 +0800

    Create a.cpp

---
+++ 1558/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+#ifdef zerol
+#define dbg(args...) do { cout << "\033[32;1m" << #args << " -> "; err(args); } while (0)
+#else
+#define dbg(...)
+#endif
+void err() { cout << "\033[39;0m" << endl; }
+template<template<typename...> class T, typename t, typename... Args>
+void err(T<t> a, Args... args) { for (auto x: a) cout << x << ' '; err(args...); }
+template<typename T, typename... Args>
+void err(T a, Args... args) { cout << a << ' '; err(args...); }
+// -----------------------------------------------------------------------------
+
+int main() {
+    int T; cin >> T;
+    while (T--) {
+        string s; cin >> s;
+        FOR (i, 0, s.length()) s[i] = tolower(s[i]);
+        if (s == "jessie") puts("Good guy!");
+        else puts("Dare you say that again?");
+    }
+}
